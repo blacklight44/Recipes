e8eb25d2f9d8aacf84298e9287803c564e507d85
blacklight44 <yakupyakut@yandex.com>
2022-07-18 22:12:48 +0300

    temlete engine ayarları

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-//const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//template engine ayarları
+const path = require("path");
+//ejs nin template engine olarak belirlenmesi
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+//viewin yolunun default halini tanımladık
+app.set("views", path.resolve(__dirname, "./src/views"));
+
+//
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+//
+app.use(express.static("public"));
+
+//serverjs
 app.listen(process.env.PORT, () => {
   console.log(`Server ${process.env.PORT} portundan ayaklandı`);
 });

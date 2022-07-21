c9997b8e653897083fbd2813c99832ccc3377dc4
blacklight44 <yakupyakut@yandex.com>
2022-07-21 16:40:56 +0300

    session yapısının kurulması

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
-
+const session = require("express-session");
 //template engine ayarları
 const path = require("path");
 //ejs nin template engine olarak belirlenmesi
@@ -19,6 +19,21 @@ app.use(express.static("public"));
 //db baglantısı
 require("./src/config/database");
 //
+//session ve flash message
+// session için mw her istekte session yoksa oluşturuluyor
+//flash ve passport.js için gerekli flash kendini session içine kor ve sonra kendini siler
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true, //başlangıçta cookie oluşturması için
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+    //session oluşturulurken db ye kayıt et
+    store: sessionStore,
+  })
+);
 //routerlar include edilir
 const authRouter = require("./src/routers/auth_router");
 app.use("/", authRouter);
